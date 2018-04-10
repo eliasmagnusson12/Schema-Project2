eeeca7fcc065d968e61cc54c7837ea85bb7ca6ef
Elias Magnusson <magnusson.elias1@gmail.com>
2018-04-11 00:51:25 +0200

    Elias testar, 1, 2, 3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,5 +4,6 @@ public class Controller {
   private String message = "Final test Filip";
   private String test = "Hejhopp";
   private String test2 = "testar";
+  private String test33 = "Elias test";
 
 }
