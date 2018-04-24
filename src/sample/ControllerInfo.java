652182d97ebf9dc14b2205ddb867e0ad3b3eaa9b
Elias Magnusson <magnusson.elias1@gmail.com>
2018-04-24 16:10:23 +0200

    Design för email, help och Info

--- src/sample/ControllerInfo.java
+++ src/sample/ControllerInfo.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class ControllerInfo {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerInfo implements Initializable {
+    @FXML
+    private AnchorPane pane;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Image image = new Image("resourses/credit.png");
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        pane.setBackground(new Background(backgroundImage));
+
+
+    }
 }
--- src/sample/ControllerMail.java
+++ src/sample/ControllerMail.java
@@ -1,4 +1,52 @@
 package sample;
 
-public class ControllerMail {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerMail implements Initializable {
+
+    @FXML
+    CheckBox box1, box2, box3, box4;
+    @FXML
+    TextArea mailTextArea;
+    @FXML
+    Button send;
+    @FXML
+    AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Image sendImage = new Image("resourses/sendButton.png");
+        ImageView sendImageView = new ImageView(sendImage);
+        send.setGraphic(sendImageView);
+        send.setStyle("-fx-background-color: TRANSPARENT");
+
+        Image smallBackgroundImage = new Image("resourses/smallBackground.jpg");
+        BackgroundImage backgroundImage = new BackgroundImage(smallBackgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        anchorPane.setBackground(new Background(backgroundImage));
+
+
+    }
+    @FXML
+    private void handleHoverEffect(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("-fx-background-color: LIGHTGRAY");
+    }
+
+    @FXML
+    private void handleNoHoverEffect(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("-fx-background-color: TRANSPARENT");
+    }
 }
