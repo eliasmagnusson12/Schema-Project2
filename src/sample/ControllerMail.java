e80f8c9bda98fec99a7efc56fcbc5f1510403300
Elias Magnusson <magnusson.elias1@gmail.com>
2018-04-25 15:02:23 +0200

    kopplat email till appen

--- src/sample/ControllerMail.java
+++ src/sample/ControllerMail.java
@@ -2,16 +2,24 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
+
 
 public class ControllerMail implements Initializable {
 
@@ -23,6 +31,10 @@ public class ControllerMail implements Initializable {
     Button send;
     @FXML
     AnchorPane anchorPane;
+    @FXML
+    TextField subject;
+    @FXML
+    Label messageSent;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -48,5 +60,63 @@ public class ControllerMail implements Initializable {
     private void handleNoHoverEffect(MouseEvent event) {
         Button button = (Button) event.getSource();
         button.setStyle("-fx-background-color: TRANSPARENT");
+    }
+    @FXML
+    public void sendEmail(){
+
+        String from = "kristianstad.gadors@hotmail.com";
+        String pass = "gadors123";
+
+        String to = "Schedule.Kristianstad@hotmail.com";
+
+        String host = "smtp.live.com";
+        Properties properties = System.getProperties();
+
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.user", from);
+        properties.put("mail.smtp.password", pass);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try{
+
+
+            MimeMessage message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(from));
+            message.addRecipients(RecipientType.TO, to);
+
+            message.setSubject(subject.getText());
+
+            message.setText(mailTextArea.getText() + "\nYou can not respond to this mail. ");
+
+
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            System.out.println("Sent message successfully....");
+
+            subject.clear();
+            mailTextArea.clear();
+            subject.setPromptText("Subject...");
+            mailTextArea.setPromptText("Message...");
+
+            messageSent.setText("Your message was sent successfully");
+
+
+
+
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
