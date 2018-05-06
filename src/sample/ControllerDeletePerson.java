14674200a37e1fc9f29bd79405836aaf80b5fe6a
Femkant <k4rstrom@hotmail.com>
2018-05-06 11:58:34 +0200

    delete person

---
+++ src/sample/ControllerDeletePerson.java
@@ -0,0 +1,70 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerDeletePerson implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Button okDeleteButton;
+    @FXML
+    private TextField ssnTextField;
+    @FXML
+    private Label okLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image mediumBackgroundImage = new Image("resourses/2.jpg");
+        BackgroundImage backgroundImage = new BackgroundImage(mediumBackgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        anchorPane.setBackground(new Background(backgroundImage));
+
+        Image checkImage = new Image("resourses/check.png");
+        ImageView checkImageView = new ImageView(checkImage);
+        okDeleteButton.setGraphic(checkImageView);
+        okDeleteButton.setStyle("-fx-background-color: TRANSPARENT");
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                okDeleteButton.requestFocus();
+            }
+        });
+    }
+
+    @FXML
+    private void handleOkDeleteButton(ActionEvent event){
+        String username = ssnTextField.getText();
+
+        DBConnect dbConnect = new DBConnect();
+        dbConnect.removePerson(username);
+
+        okLabel.setText("Done!");
+
+    }
+
+    @FXML
+    private void handleHoverEffect(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("-fx-background-color: LIGHTGRAY");
+    }
+
+    @FXML
+    private void handleNoHoverEffect(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("-fx-background-color: TRANSPARENT");
+    }
+}
